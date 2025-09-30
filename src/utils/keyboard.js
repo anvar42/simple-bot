a8efdf5b87ad64b44364d91150cd06fd684ef151
Anvar571 <abdurashidovanvar52@gmail.com>
2025-10-01 00:21:25 +0500

    fead: add deploy flow

--- src/utils/keyboard.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mainMenuKeyboard = void 0;
-exports.mainMenuKeyboard = {
-    inline_keyboard: [
-        [{ text: '📜 Qoidalar', callback_data: 'rules' }],
-        [{ text: 'ℹ️ Bot haqida', callback_data: 'about' }],
-    ],
-};
